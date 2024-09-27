b340d405026428807b288ae2a3723ee8a1d589a7
Justin Gardner <justin@rhynorater.com>
2024-09-27 06:03:31 -0500

    5.0.12

--- download-monitor/vendor/composer/installed.php
+++ download-monitor/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'wpchill/download-monitor',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'b593f35a22be181bf279cfdc22f124728501317a',
+        'reference' => '4c901cd8b787945eee328f433b77cb8db773b1d0',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'wpchill/download-monitor' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'b593f35a22be181bf279cfdc22f124728501317a',
+            'reference' => '4c901cd8b787945eee328f433b77cb8db773b1d0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
